5c1f8a2908db8ce504acebc60d2d9e0244f28d3f
Jabrail Atakishiyev <cebrayil.atakishiyev@gmail.com>
2025-11-10 22:45:53 +0400

    Updated Service layer code

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26002575db84eae0a728ec3fe0a4ed960cbbd12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7510a7650f360064e5971ca798b74054983d311f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
