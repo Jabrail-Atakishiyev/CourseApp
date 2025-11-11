40a97dc5fa4e8142234911cf33b3a99ad4799c07
Jabrail Atakishiyev <cebrayil.atakishiyev@gmail.com>
2025-11-12 00:54:57 +0400

    Updated Presentation layer code

--- CourseApp/obj/Debug/net8.0/CourseApp.AssemblyInfo.cs
+++ CourseApp/obj/Debug/net8.0/CourseApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26002575db84eae0a728ec3fe0a4ed960cbbd12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14db3b4928767061d067abc5a6a23c92c291c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
